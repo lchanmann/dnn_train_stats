369ae69f8c91443dd7f8085097714b5f2a3ef8db
Mann <chanmannlim@gmail.com>
2016-02-24 14:18:12 -0600

    add data and plot x-entropy for all depth

--- plot_entropy.m
+++ plot_entropy.m
@@ -1,34 +1,38 @@
 % global constants
-nH = '800';                                 % number of hidden units
+nH = 800:200:2000;                          % number of hidden units
 colors = {'b-', 'g--', 'r-.', 'm:', 'c-'};  % colors for ploting
 dnns = 3:7;                                 % dnn models
 
 % variables
 l = length(dnns);   % total models to be evaluated
-M = cell(1, l);     % legend text
-x = zeros(2, l);    % finetune points
 
-% plot
-figure; hold on
+for i=1:length(nH)
+    M = cell(1, l);         % legend text
+    x = zeros(2, l);        % finetune points
+    nodes = num2str(nH(i)); % number of hidden nodes
 
-% Go through DNN models with different depth
-for j=1:l
-    i = num2str(dnns(j));
-    % Load data
-    [val_entropy, stage] ...
-        = textread(['dnn' i '_' nH '.data'], '%*f,%*f,%*f,%f,%*f,%d');
-    
-    plot(val_entropy, colors{j});
-    
-    M{j} = ['DNN ' i];
-    x(1, j) = find(stage == 2, 1);
-    x(2, j) = val_entropy(x(1, j));
+    % plot
+    figure; hold on
+
+    % Go through DNN models with different depth
+    for j=1:l
+        k = num2str(dnns(j));
+        % Load data
+        [val_entropy, stage] ...
+            = textread(['dnn' k '_' nodes '.data'], '%*f,%*f,%*f,%f,%*f,%d');
+
+        plot(val_entropy, colors{j});
+
+        M{j} = ['DNN ' k];
+        x(1, j) = find(stage == 2, 1);
+        x(2, j) = val_entropy(x(1, j));
+    end
+    plot(x(1, :), x(2, :), 'kx');
+
+    % plot configurations
+    xlim([1 Inf]);
+    xlabel('# of Epoch');
+    ylabel('J/n');
+    title(['DNN with (' nodes ' hidden units) validation cross entropy']);
+    legend(M);
 end
-plot(x(1, :), x(2, :), 'kx');
-
-% plot configurations
-xlim([1 Inf]);
-xlabel('# of Epoch');
-ylabel('J/n');
-title(['DNN with (' nH ' hidden units) validation cross entropy']);
-legend(M);
